7efa7caee9da72b361011c4d76f72ba0adde170d
thestellarl <lucas.stella13@gmail.com>
2021-10-27 00:02:13 +0100

    working on machine

--- test.py
+++ test.py
@@ -7,14 +7,34 @@ from threading import Thread
 import queue
 sh = gc.open("End Matcher Board Counter")
 
+'''
 def push_to_sheets(data):
   sh.values_append('Sheet2!A1', params={'valueInputOption': 'RAW'}, body={ 'values': [[x] for x in data]})
-
-ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=0, parity=serial.PARITY_EVEN, rtscts=1)
+'''
+ser = serial.Serial('/dev/ttyUSB0', 9600, bytesize=serial.SEVENBITS, timeout=None, parity=serial.PARITY_EVEN, rtscts=1, dsrdtr=1)
 
 def process_serial():
-  
-
+	board_index = -1
+	while True:
+		a = ser.read_until(expected=b'\x03')
+		packet_start = a.find(b'\x02')
+		hex_string = a[packet_start + 1:-1].decode('utf-8')
+		hex_flipped = "".join(reversed([hex_string[i:i+2] for i in range(0, len(hex_string), 2)]))
+		if len(hex_flipped) == 108:
+			board_num = int(hex_flipped[-8:], 16)
+			board_len = int(hex_flipped[:8], 16)
+			if board_index == -1 or board_num != board_index:
+				board_index = board_num
+				print('board num', int(hex_flipped[-8:], 16))
+				print('board len', int(hex_flipped[:8], 16)/100)
+				length = board_len/100
+				id = uuid.uuid4()
+				sh.values_append('Sheet2!A1', params={'valueInputOption': 'RAW'}, body={ 'values': [[str(id), length]]})
+				socketio.send(f"{{\"id\":\"{id}\", \"length\":{length}}}")
+				#hex_flipped = "".join(reversed([b[i:i+2] for i in range(0, len(b), 2)]))
+				#print(hex_flipped)
+				# print(int(hex_flipped, 16))
+'''
   for x in range(20):
     data_queue.put(x)
     time.sleep(1)
@@ -23,6 +43,7 @@ def process_serial():
     sh.values_append('Sheet2!A1', params={'valueInputOption': 'RAW'}, body={ 'values': [[str(id), length]]})
     socketio.send(f"{{\"id\":\"{id}\", \"length\":{length}}}")
   return
+'''
 
 def handle_remove(id):
   ws = sh.get_worksheet_by_id(1111478912)
